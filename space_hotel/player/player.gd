19c882203aa74e36eab42ec87da194392c745678
Ryan <bett8282@gmail.com>
2024-10-06 12:05:26 -0400

    Changed input method to begin dialog to is_anything_pressed

--- space_hotel/player/player.gd
+++ space_hotel/player/player.gd
@@ -5,7 +5,7 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 func _unhandled_input(_event: InputEvent) -> void:
-	if (Input.is_action_just_pressed("ui_accept")):
+	if (Input.is_anything_pressed()):
 		DialogueManager.show_example_dialogue_balloon(load("res://dialogue/main.dialogue"), "start")
 		return
 		
@@ -16,7 +16,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_anything_pressed() and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
