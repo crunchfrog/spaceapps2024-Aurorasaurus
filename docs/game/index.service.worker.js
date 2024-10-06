91c68692a7b595f7906b3275b9e02df1571ebf37
amer <amer.bt@gmail.com>
2024-10-06 17:18:13 -0400

    release version of scene change

--- docs/game/index.service.worker.js
+++ docs/game/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1728247477|1685984531';
+const CACHE_VERSION = '1728249472|3681378041';
 /** @type {string} */
 const CACHE_PREFIX = 'mySpace-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
