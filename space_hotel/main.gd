39c150da7b06364771b5d31d52475504f546a4de
amer <amer.bt@gmail.com>
2024-10-06 12:53:52 -0400

    added empty earth scene.

--- space_hotel/main.gd
+++ space_hotel/main.gd
@@ -7,3 +7,7 @@ func _on_quit_pressed() -> void:
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://world.tscn")
+
+
+func _on_play_earth_pressed() -> void:
+	get_tree().change_scene_to_file("res://earth_01.tscn")
