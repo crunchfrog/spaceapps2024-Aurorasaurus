b4d550d368b7b94bdb7642ea3db0ecea72b63995
Ryan <bett8282@gmail.com>
2024-10-06 12:34:58 -0400

    Added sprite flip for movement direction.

--- space_hotel/player/player.gd
+++ space_hotel/player/player.gd
@@ -5,6 +5,7 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 @onready var actionable_finder: Area2D = $Direction/ActionableFinder
+@onready var sprite := get_node("AnimatedSprite2D")
 
 
 func _unhandled_input(_event: InputEvent) -> void:
@@ -28,6 +29,13 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	# Flip the sprite based off direction.
+	if (direction > 0):
+		sprite.flip_h = true 
+	if (direction < 0):
+		sprite.flip_h = false 
+		
 	if direction:
 		velocity.x = direction * SPEED
 	else:
