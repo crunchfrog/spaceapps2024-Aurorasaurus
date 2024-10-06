b101d56036f0ef84a185992c84062d64766b765f
amer <amer.bt@gmail.com>
2024-10-06 17:44:19 -0400

    replaced background for hotel room

--- docs/game/index.service.worker.js
+++ docs/game/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1728249472|3681378041';
+const CACHE_VERSION = '1728251038|5247338210';
 /** @type {string} */
 const CACHE_PREFIX = 'mySpace-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
